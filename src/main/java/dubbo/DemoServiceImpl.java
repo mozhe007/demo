bead0a7445ddc9236c92fdfb04b2cd5fcec6f526
mozhe007 <mozhe007@qq.com>
2018-04-12 22:36:11 +0800

    #3 dubbo的hello world

---
+++ src/main/java/dubbo/DemoServiceImpl.java
@@ -0,0 +1,7 @@
+package dubbo;
+
+public class DemoServiceImpl implements DemoService  {
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+}
---
+++ src/main/java/dubbo/Provider.java
@@ -0,0 +1,14 @@
+package dubbo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Provider {
+
+    public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                new String[] {"classpath*:spring-dubbo-provider.xml"});
+        context.start();
+        // press any key to exit
+        System.in.read();
+    }
+}
