026cb4910ae54604c20d0ccadfe99e8cd2440e6e
mozhe007 <mozhe007@qq.com>
2019-11-04 23:49:54 +0800

    #优化目录

--- src/test/java/jdk/multithread/MyCallableTest.java
+++
@@ -1,9 +0,0 @@
-package jdk.multithread;
-
-public class MyCallableTest {
-
-    public static void main(String[] args) {
-        MyCallable myCallable = new MyCallable();
-        myCallable.callable();
-    }
-}
