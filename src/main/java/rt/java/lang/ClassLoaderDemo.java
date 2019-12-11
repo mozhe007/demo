a4eae40c35c095e521775a140d8318f7cd3922ea
mozhe007 <mozhe007@qq.com>
2019-12-11 21:13:06 +0800

    #破坏双亲加载

---
+++ src/main/java/rt/java/lang/ClassLoaderDemo.java
@@ -0,0 +1,36 @@
+package rt.java.lang;
+
+
+public class ClassLoaderDemo extends ClassLoader {
+
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        //  偷偷地替换对String的加载。
+        if (name.equals("java.lang.String")) {
+            return loadClass("commonbean.Fake", false);
+        }
+        return loadClass(name, false);
+    }
+
+    // 尝试破环双亲加载机制
+    public void brokeParentsLoading() {
+        ClassLoaderDemo classLoaderDemo = new ClassLoaderDemo();
+        Thread.currentThread().setContextClassLoader(classLoaderDemo);
+        try {
+            Class<?> aClass = Thread.currentThread().getContextClassLoader().loadClass("java.lang.String");
+            System.out.println(aClass.newInstance().toString());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        ClassLoaderDemo classLoaderDemo = new ClassLoaderDemo();
+        classLoaderDemo.brokeParentsLoading();
+    }
+
+}
